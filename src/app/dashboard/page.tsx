8e8aa80afb4aabc29436919171f4ec8f846b0563
Pedro Leonardo <pedroleon.pc@hotmail.com>
2023-09-25 18:22:48 -0300

    Add TailWind CSS

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,30 @@
+
+export default function Dashboard() {
+    return (
+        <main>
+            <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
+                <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
+                <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
+                <div className="mx-auto max-w-2xl lg:max-w-4xl">
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                        Olá, <a href="#" className="special">Pedro</a>
+                    </h2>
+                    <figure className="mt-10">
+                        <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
+                            <p>
+                                Essa aplicação de autenticação serve para demonstrar meus conhecimentos, feita 100% por <a href="https://www.linkedin.com/in/pedroleodev/" target="_blank" className="special">Pedro Leonardo</a>. No Front-End utilizei: TypeScript, React.Js, Next.Js e TailWind CSS, e no Back-End: Node.Js e MongoDB.
+                            </p>
+                        </blockquote>
+                    </figure>
+                </div>
+                <div>
+                    <button
+                        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 max-width"
+                    >
+                        Sair
+                    </button>
+                </div>
+            </section>
+        </main>
+    )
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,89 @@
+'use client'
+import { useState } from "react"
+
+export default function Login() {
+
+    const [emailUser, setEmailUser] = useState<string>('')
+    const [passwordUser, setPasswordUser] = useState<string>('')
+
+    async function LoginUser() {
+        alert('ok')
+    }
+
+    return (
+        <main className="container_background">
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                        Entrar com sua conta
+                    </h2>
+                </div>
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-6" action="#" onSubmit={LoginUser} method="POST">
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                Email:
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="email"
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    value={emailUser}
+                                    onChange={(e) => setEmailUser(e.target.value)}
+                                    placeholder="Digite seu email"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Senha:
+                                </label>
+                                <div className="text-sm">
+                                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                                        Esqueceu sua senha?
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    value={passwordUser}
+                                    onChange={(e) => setPasswordUser(e.target.value)}
+                                    placeholder="Digite sua senha"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                               Entrar
+                            </button>
+                        </div>
+                    </form>
+
+                    <p className="mt-10 text-center text-sm text-gray-500">
+                        Não tem uma conta ?{' '}
+                        <a href="/register" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                            Registre-se
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </main>
+    )
+}
